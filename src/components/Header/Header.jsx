2d65a850fb4600e2d192221f89f6b6a2581dcc4a
wjbrower <40956825+wjbrower@users.noreply.github.com>
2018-09-20 18:36:38 -0400

    a

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -71,7 +71,7 @@ class Header extends React.Component {
       <AppBar className={appBarClasses}>
         <Toolbar className={classes.container}>
           <Button className={classes.title}>
-          <img src={logo} />
+          <img src={logo} style={{height: "auto", width: "50px"}}/>
             <Link to="/">{brand}</Link>
           </Button>
           <Hidden smDown implementation="css" className={classes.hidden}>
