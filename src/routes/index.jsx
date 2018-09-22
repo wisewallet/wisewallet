f3c1adf8330894d340a437f7c45596409b0ccc44
wjbrower <40956825+wjbrower@users.noreply.github.com>
2018-09-22 17:19:57 -0400

    Signuppage

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,8 +1,9 @@
 import LandingPage from "views/LandingPage/LandingPage.jsx";
 import LoginPage from "views/LoginPage/LoginPage.jsx";
-
+import SignupPage from "views/SignupPage/SignupPage.jsx";
 var indexRoutes = [
   { path: "/login", name: "Login", component: LoginPage },
+  { path: "/signup", name: "WiseWallet", component: SignupPage },
   { path: "/", name: "WiseWallet", component: LandingPage }
 ];
 
