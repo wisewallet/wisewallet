9033dce6ac665e6e1957f37a47b9ece718ac9ef1
wjbrower <40956825+wjbrower@users.noreply.github.com>
2018-09-25 14:10:28 -0400

    a

--- src/views/LandingPage/LandingPage.jsx
+++ src/views/LandingPage/LandingPage.jsx
@@ -75,26 +75,132 @@ class LandingPage extends React.Component {
           </div>
         </div>
         <Footer
+          theme="white"
           content={
             <div>
-              <div className={classes.left}>
-                <List className={classes.list}>
-                  <ListItem className={classes.inlineBlock}>
-                    <a
-                      href="https://medium.com/@wisewallet"
-                      className={classes.block}
-                    >
-                      Blog
-                    </a>
-                  </ListItem>
-                </List>
-              </div>
-              <div className={classes.right}>
-                WiseWallet, Inc. &copy; 2018 All Rights Reserved.
+              <ul className={classes.socialButtons}>
+                <li>
+                  <Button justIcon simple href="#pablo" color="twitter">
+                    <i className="fab fa-twitter" />
+                  </Button>
+                </li>
+                <li>
+                  <Button justIcon simple href="#pablo" color="facebook">
+                    <i className="fab fa-facebook-square" />
+                  </Button>
+                </li>
+                <li>
+                  <Button justIcon simple href="#pablo" color="dribbble">
+                    <i className="fab fa-dribbble" />
+                  </Button>
+                </li>
+                <li>
+                  <Button justIcon simple href="#pablo" color="google">
+                    <i className="fab fa-google-plus-g" />
+                  </Button>
+                </li>
+                <li>
+                  <Button justIcon simple href="#pablo" color="youtube">
+                    <i className="fab fa-youtube" />
+                  </Button>
+                </li>
+              </ul>
+              <div
+                className={classNames(classes.pullCenter, classes.copyRight)}
+              >
+                Copyright &copy; {1900 + new Date().getYear()}{" "}
+                <a href="https://www.creative-tim.com">Creative Tim</a> All
+                Rights Reserved.
               </div>
             </div>
           }
-        />
+        >
+          <div className={classes.footer}>
+            <GridContainer>
+              <GridItem xs={12} sm={3} md={3}>
+                <a href="#pablo">
+                  <h5>Material Kit PRO</h5>
+                </a>
+                <p>
+                  Probably the best UI Kit in the world! We know you've been
+                  waiting for it, so don't be shy!
+                </p>
+              </GridItem>
+              <GridItem xs={12} sm={2} md={2}>
+                <h5>About</h5>
+                <ul className={classes.linksVertical}>
+                  <li>
+                    <a href="#pablo">Blog</a>
+                  </li>
+                  <li>
+                    <a href="#pablo">About us</a>
+                  </li>
+                  <li>
+                    <a href="#pablo">Presentation</a>
+                  </li>
+                  <li>
+                    <a href="#pablo">Contact us</a>
+                  </li>
+                </ul>
+              </GridItem>
+              <GridItem xs={12} sm={2} md={2}>
+                <h5>About</h5>
+                <ul className={classes.linksVertical}>
+                  <li>
+                    <a href="#pablo">Sales FAQ</a>
+                  </li>
+                  <li>
+                    <a href="#pablo">How to register</a>
+                  </li>
+                  <li>
+                    <a href="#pablo">Sell Goods</a>
+                  </li>
+                  <li>
+                    <a href="#pablo">Receive Payment</a>
+                  </li>
+                  <li>
+                    <a href="#pablo">Transactions Issues</a>
+                  </li>
+                </ul>
+              </GridItem>
+              <GridItem xs={12} sm={2} md={2}>
+                <h5>About</h5>
+                <ul className={classes.linksVertical}>
+                  <li>
+                    <a href="#pablo">Transactions FAQ</a>
+                  </li>
+                  <li>
+                    <a href="#pablo">Terms & conditions</a>
+                  </li>
+                  <li>
+                    <a href="#pablo">Licenses</a>
+                  </li>
+                </ul>
+              </GridItem>
+              <GridItem xs={12} sm={3} md={3}>
+                <h5>Subscribe to Newsletter</h5>
+                <p>
+                  Join our newsletter and get news in your inbox every week! We
+                  hate spam too, so no worries about this.
+                </p>
+                <form>
+                  <CustomInput
+                    id="footeremail"
+                    formControlProps={{
+                      fullWidth: false
+                    }}
+                    inputProps={{
+                      placeholder: "Your Email..."
+                    }}
+                  />
+                  <Button color="primary" justIcon>
+                    <Mail />
+                  </Button>
+                </form>
+              </GridItem>
+            </GridContainer>
+          </div>
+        </Footer>
       </div>
     );
   }
