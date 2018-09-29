5e06ef9abb8c5611d970af7704c3f0279e7e8db5
wjbrower <40956825+wjbrower@users.noreply.github.com>
2018-09-29 11:18:04 -0400

    a

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,13 @@
 import LandingPage from "views/LandingPage/LandingPage.jsx";
 import LoginPage from "views/LoginPage/LoginPage.jsx";
 import SignupPage from "views/SignupPage/SignupPage.jsx";
+import PlaidLink from "views/PlaidLink/PlaidLink.jsx";
+
 var indexRoutes = [
-  { path: "/login", name: "Login", component: LoginPage },
-  { path: "/signup", name: "WiseWallet", component: SignupPage },
-  { path: "/", name: "WiseWallet", component: LandingPage }
+	{path: "/login", name: "Login", component: LoginPage},
+	{path: "/signup", name: "WiseWallet", component: SignupPage},
+	{path: "/plaidlink", name: "WiseWallet", component: PlaidLink},
+	{path: "/", name: "WiseWallet", component: LandingPage}
 ];
 
 export default indexRoutes;
