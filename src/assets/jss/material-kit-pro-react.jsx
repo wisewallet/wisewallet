73460d155c6611d57aba94109f04324b645db5e0
wjbrower <40956825+wjbrower@users.noreply.github.com>
2018-10-03 13:24:57 -0400

    a

--- src/assets/jss/material-kit-pro-react.jsx
+++ src/assets/jss/material-kit-pro-react.jsx
@@ -79,12 +79,10 @@ const roseColor = "#e91e63";
 const grayColor = "#999999";
 
 const primaryBoxShadow = {
-  boxShadow:
-    "0 12px 20px -10px rgba(156, 39, 176, 0.28), 0 4px 20px 0px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(156, 39, 176, 0.2)"
+
 };
 const infoBoxShadow = {
-  boxShadow:
-    "0 12px 20px -10px rgba(0, 188, 212, 0.28), 0 4px 20px 0px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(0, 188, 212, 0.2)"
+
 };
 const successBoxShadow = {
   boxShadow:
@@ -120,12 +118,12 @@ const dangerCardHeader = {
 };
 const infoCardHeader = {
   color: "#fff",
-  background: "linear-gradient(60deg, #26c6da, #00acc1)",
+  background: "#6bafe0",
   ...infoBoxShadow
 };
 const primaryCardHeader = {
   color: "#fff",
-  background: "linear-gradient(60deg, #ab47bc, #8e24aa)",
+  background: "#6bafe0",
   ...primaryBoxShadow
 };
 const roseCardHeader = {
