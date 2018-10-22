15dea0ded75724f8f662088a6fc082b481f3e975
William Brower <williamjbrower@gmail.com>
2018-10-21 20:20:40 -0400

    a

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,12 +3,13 @@ import LoginPage from "views/LoginPage/LoginPage.jsx";
 import SignupPage from "views/SignupPage/SignupPage.jsx";
 import PlaidLink from "views/PlaidLink/PlaidLink.jsx";
 import Dashboard from "views/Dashboard/Dashboard.jsx";
-
+import Charts deom "view/Charts/Charts.jsx";
 var indexRoutes = [
 	{path: "/login", name: "Login", component: LoginPage},
 	{path: "/signup", name: "WiseWallet", component: SignupPage},
 	{path: "/link", name: "WiseWallet", component: PlaidLink},
 	{path: "/dashboard", name: "WiseWallet", component: Dashboard},
+	{path: "/charts", name: "WiseWallet", component: Charts},
 	{path: "/", name: "WiseWallet", component: LandingPage}
 ];
 
