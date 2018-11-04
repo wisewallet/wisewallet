bc54acef73be1b65e1c2ad240b13a34bf5537f68
William Brower <will@mywisewallet.com>
2018-11-04 14:34:56 -0500

    Plaid Link

--- src/views/PlaidLink/PlaidLink.jsx
+++ src/views/PlaidLink/PlaidLink.jsx
@@ -13,132 +13,184 @@ import Card from "components/Card/Card.jsx";
 import CardBody from "components/Card/CardBody.jsx";
 import plaidLinkStyle from "assets/jss/material-kit-pro-react/views/plaidLinkStyle.jsx";
 import PlaidLink from "react-plaid-link";
-import { Redirect } from 'react-router';
+import {
+  Redirect
+} from 'react-router';
 
 import image from "assets/img/bg7.jpg";
 
 class Components extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			email: "",
-			password: "",
-			firstName: "",
-			lastName: "",
-			userID: ""
-		};
-		this.onSignUp = this.onSignUp.bind(this);
-		this.handleOnSuccess = this.handleOnSuccess.bind(this);
-	}
-	componentDidMount() {
-		window.scrollTo(0, 0);
-		document.body.scrollTop = 0;
-		var userID = sessionStorage.getItem("userID");
-		this.setState({
-			userID: userID
-		});
-	}
-	handleChange = name => event => {
-		this.setState({
-			[name]: event.target.value
-		});
-	};
-	onSignUp() {
-		const {email, password, firstName, lastName} = this.state;
-		fetch(
-			"https://tldpv6umn7.execute-api.us-east-1.amazonaws.com/default/signUp",
-			{
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json"
-				},
-				body: JSON.stringify({
-					email: email,
-					lastName: lastName,
-					password: password,
-					firstName: firstName
-				})
-			}
-		)
-			.then(response => response.json())
-			.then(json => console.log(json));
-	}
-	handleOnSuccess(token, metadata) {
-		console.log("Plaid Token: " + token);
-		this.props.history.push('/dashboard');
-	}
-	handleOnExit() {
-		// handle the case when your user exits Link
-	}
-	render() {
-		const {classes, ...rest} = this.props;
-		return (
-			<div>
-				<Header
-					absolute="absolute"
-					color="transparent"
-					brand="WiseWallet"
-					{...rest}
-				/>
-				<div
-					className={classes.pageHeader}
-					style={{
-						backgroundImage: "url(" + image + ")",
-						backgroundSize: "cover",
-						backgroundPosition: "top center"
-					}}
-				>
-					<div className={classes.container}>
-						<GridContainer justify="center">
-							<GridItem xs={12} sm={6} md={6}>
-								<Card className={classes.cardSignup}>
-									<h2 className={classes.cardTitle}>Link Your Bank Account</h2>
-									<CardBody>
-										<GridContainer justify="center">
-											<GridItem xs={12} sm={5} md={5}>
-												<PlaidLink
-													clientName="WiseWallet"
-													env="sandbox"
-													product={["auth", "transactions"]}
-													publicKey="06812b585d6f3b0ebde352a7759bb1"
-													onExit={this.handleOnExit}
-													onSuccess={this.handleOnSuccess}
-												>
-													Open Link and connect your bank!
-												</PlaidLink>
-											</GridItem>
-										</GridContainer>
-									</CardBody>
-								</Card>
-							</GridItem>
-						</GridContainer>
-					</div>
-					<Footer
-						content={
-							<div>
-								{" "}
-								<div className={classes.left}>
-									<List className={classes.list}>
-										<ListItem className={classes.inlineBlock}>
-											<a
-												href="https://medium.com/@wisewallet"
-												className={classes.block}
-											>
-												Blog
-											</a>
-										</ListItem>
-									</List>
-								</div>
-								<div className={classes.right}>
-									WiseWallet, Inc. &copy; 2018 All Rights Reserved.
-								</div>
-							</div>
-						}
-					/>
-				</div>
-			</div>
-		);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      firstName: "",
+      lastName: "",
+      userID: ""
+    };
+    this.onSignUp = this.onSignUp.bind(this);
+    this.handleOnSuccess = this.handleOnSuccess.bind(this);
+  }
+  componentDidMount() {
+    window.scrollTo(0, 0);
+    document.body.scrollTop = 0;
+    var userID = sessionStorage.getItem("userID");
+    this.setState({
+      userID: userID
+    });
+  }
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value
+    });
+  };
+  handleOnSuccess(token, metadata) {
+    console.log("Plaid Token: " + token);
+    const {
+      userID
+    } = this.state;
+    fetch(
+        "https://wu41guprla.execute-api.us-east-1.amazonaws.com/default/linkPlaid", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            userID: userID,
+						public_token: token
+          })
+        }
+      )
+      .then(response => response.json())
+      .then(json => console.log(json));
+    this.props.history.push('/dashboard');
+  }
+  handleOnExit() {
+    // handle the case when your user exits Link
+  }
+  render() {
+    const {
+      classes,
+      ...rest
+    } = this.props;
+    return ( <
+      div >
+      <
+      Header absolute = "absolute"
+      color = "transparent"
+      brand = "WiseWallet" { ...rest
+      }
+      /> <
+      div className = {
+        classes.pageHeader
+      }
+      style = {
+        {
+          backgroundImage: "url(" + image + ")",
+          backgroundSize: "cover",
+          backgroundPosition: "top center"
+        }
+      } >
+      <
+      div className = {
+        classes.container
+      } >
+      <
+      GridContainer justify = "center" >
+      <
+      GridItem xs = {
+        12
+      }
+      sm = {
+        6
+      }
+      md = {
+        6
+      } >
+      <
+      Card className = {
+        classes.cardSignup
+      } >
+      <
+      h2 className = {
+        classes.cardTitle
+      } > Link Your Bank Account < /h2> <
+      CardBody >
+      <
+      GridContainer justify = "center" >
+      <
+      GridItem xs = {
+        12
+      }
+      sm = {
+        5
+      }
+      md = {
+        5
+      } >
+      <
+      PlaidLink clientName = "WiseWallet"
+      env = "sandbox"
+      product = {
+        ["auth", "transactions"]
+      }
+      publicKey = "06812b585d6f3b0ebde352a7759bb1"
+      onExit = {
+        this.handleOnExit
+      }
+      onSuccess = {
+        this.handleOnSuccess
+      } >
+      Open Link and connect your bank!
+      <
+      /PlaidLink> <
+      /GridItem> <
+      /GridContainer> <
+      /CardBody> <
+      /Card> <
+      /GridItem> <
+      /GridContainer> <
+      /div> <
+      Footer content = { <
+        div > {
+          " "
+        } <
+        div className = {
+          classes.left
+        } >
+        <
+        List className = {
+          classes.list
+        } >
+        <
+        ListItem className = {
+          classes.inlineBlock
+        } >
+        <
+        a
+        href = "https://medium.com/@wisewallet"
+        className = {
+          classes.block
+        } >
+        Blog <
+        /a> <
+        /ListItem> <
+        /List> <
+        /div> <
+        div className = {
+          classes.right
+        } >
+        WiseWallet,
+        Inc. & copy;2018 All Rights Reserved. <
+        /div> <
+        /div>
+      }
+      /> <
+      /div> <
+      /div>
+    );
+  }
 }
 
 export default withStyles(plaidLinkStyle)(Components);
