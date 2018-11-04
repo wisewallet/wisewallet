0fe7c7dee47a2132b7adf5335b260fd486b2e39a
William Brower <will@mywisewallet.com>
2018-11-04 15:40:00 -0500

    a

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -64,7 +64,7 @@ class Dashboard extends React.Component {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({userID: sessionStorage.getItem("userID")})
-    }).then(response => console.log(response));
+    }).then(response => console.log(response.json()));
   }
   componentDidMount() {
     window.scrollTo(0, 0);
