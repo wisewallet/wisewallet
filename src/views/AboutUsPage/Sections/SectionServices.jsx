453bf2c156eb571997a9bf1d965747c10d1f98eb
William Brower <will@mywisewallet.com>
2018-11-18 13:32:57 -0500

    change language

--- src/views/AboutUsPage/Sections/SectionServices.jsx
+++ src/views/AboutUsPage/Sections/SectionServices.jsx
@@ -18,40 +18,38 @@ function SectionServices(props) {
   return (<div className={classes.services}>
     <GridContainer>
       <GridItem md={8} sm={8} className={classNames(classes.mlAuto, classes.mrAuto, classes.textCenter)}>
-        <h2 className={classes.title}>Our Mission</h2>
-        <h5 className={classes.description}>
-          We believe that we as consumers should be aware of the policies and actions of companies with whom we spend our money with. If we support companies who are doing what is right for their employees, their communities, and the environment, this positive reinforcement can make the world a better place with each dollar we spend.
-        </h5>
-      </GridItem>
-      <GridItem md={8} sm={8} className={classNames(classes.mlAuto, classes.mrAuto, classes.textCenter)}>
         <h2 className={classes.title}>What We Do</h2>
-        <h5 className={classes.description}>
-          WiseWallet provides a Total Impact Score based on your daily spending. By collecting and analyzing third party research, we procure the most accurate environmental, social, and corporate governance ratings of companies. These ratings are matched to your daily transactions to provide your custom score. WiseWallet aims to make consumers aware of their impact and hopes to empower our users to spend with companies that are environmentally friendly, socially progressive, and ethical.
-        </h5>
+        <h4 className={classes.description}>
+          WiseWallet is a first of its kind impact spending tool designed to inform users about how their spending aligns to their values. What if we as consumers knew exactly what we were supporting with each purchase? Our free application delivers this transparency through personalized scores based on your impact on people, the planet, corporate policy and politics quantified through your everyday purchases. By bridging the information gap, we enable you to take action with each transaction.</h4>
       </GridItem>
       <GridItem md={8} sm={8} className={classNames(classes.mlAuto, classes.mrAuto, classes.textCenter)}>
         <h2 className={classes.title}>What is my Total Impact Score?</h2>
-        <h5 className={classes.description}>
+        <h4 className={classes.description}>
           Your Total Impact Score is a combination of environmental, social, and corporate governance ratings. All WiseWallet scores are based on a quantitative score on a scale of 1-100.
-        </h5>
+        </h4>
       </GridItem>
       <GridItem md={8} sm={8} className={classNames(classes.mlAuto, classes.mrAuto, classes.textCenter)}>
-        <h2 className={classes.title}>What factors are included in my environmental score?</h2>
-        <h5 className={classes.description}>
+        <h2 className={classes.title}>What factors are included in my Planet score?</h2>
+        <h4 className={classes.description}>
           Resource use, emissions, innovation, climate change/carbon, green energy use, and more.
-        </h5>
+        </h4>
       </GridItem>
       <GridItem md={8} sm={8} className={classNames(classes.mlAuto, classes.mrAuto, classes.textCenter)}>
-        <h2 className={classes.title}>What factors are included in my social score?</h2>
-        <h5 className={classes.description}>
+        <h2 className={classes.title}>What factors are included in my People score?</h2>
+        <h4 className={classes.description}>
           Human rights, diversity and inclusion, workforce treatment and safety, community outreach, product responsibility, animal welfare, and more.
-        </h5>
+        </h4>
       </GridItem>
       <GridItem md={8} sm={8} className={classNames(classes.mlAuto, classes.mrAuto, classes.textCenter)}>
-        <h2 className={classes.title}>What factors are included in my governance score?</h2>
-        <h5 className={classes.description}>
+        <h2 className={classes.title}>What factors are included in my Policy score?</h2>
+        <h4 className={classes.description}>
           Management structure and compensation, board diversity, anti-corruption policies, employee relations, Corporate Social Responsibility strategy, and more.
-        </h5>
+        </h4>
+      </GridItem>
+      <GridItem md={8} sm={8} className={classNames(classes.mlAuto, classes.mrAuto, classes.textCenter)}>
+        <h2 className={classes.title}>What is my Politics Score?</h2>
+        <h4 className={classes.description}>
+          WiseWallet leverages the nonpartisan non-profit Center for Responsive Politics to track the effects of money and lobbying on elections and public policy. Any organization whose PACs, individual members, employees, owners, and those individuals’ immediate families who combined donated in excess of 60% of the organizations’ total donations to Republican or Democratic candidates in 2018 is deemed “Conservative” or “Liberal”, respectively.</h4>
       </GridItem>
     </GridContainer>
   </div>);
