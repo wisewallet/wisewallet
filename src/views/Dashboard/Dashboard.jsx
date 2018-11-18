6d11e49049a83ec9c6dd35a89c961de08f76b9e8
William Brower <will@mywisewallet.com>
2018-11-18 12:26:52 -0500

    added tooltips

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Redirect } from "react-router";
+import {Redirect} from "react-router";
 // nodejs library that concatenates classes
 
 // react component used to create nice image meadia player
@@ -19,7 +19,7 @@ import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import SnackbarContent from "@material-ui/core/SnackbarContent";
-
+import ReactTooltip from "react-tooltip";
 // import logo from "assets/img/logo.png";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import LinearProgress from "@material-ui/core/LinearProgress";
@@ -46,17 +46,14 @@ class Dashboard extends React.Component {
     });
   };
   componentWillMount() {
-    fetch(
-      "https://b87812vlr2.execute-api.us-east-1.amazonaws.com/default/scorecalc",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ userID: sessionStorage.getItem("userID") })
-        // body: JSON.stringify({userID: "5f61cc8a-1cc7-48b1-b4bb-e8f3ab318bc3"})
-      }
-    ).then(response => {
+    fetch("https://b87812vlr2.execute-api.us-east-1.amazonaws.com/default/scorecalc", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({userID: sessionStorage.getItem("userID")})
+      // body: JSON.stringify({userID: "5f61cc8a-1cc7-48b1-b4bb-e8f3ab318bc3"})
+    }).then(response => {
       response.json().then(json => {
         console.log(json);
         this.setState({
@@ -82,7 +79,7 @@ class Dashboard extends React.Component {
     const classes = this.props;
     var userID = sessionStorage.getItem("userID");
     if (userID == null) {
-      return <Redirect to="/login" />;
+      return <Redirect to="/login"/>;
     }
     // get data Here
 
@@ -92,129 +89,90 @@ class Dashboard extends React.Component {
       minimumFractionDigits: 2
     });
     console.log(sessionStorage);
-    return (
-      <div className={classes.dashboard}>
+    return (<div className={classes.dashboard}>
 
-        <AppHeader
-          brand="WiseWallet"
-          links={ <Button
-              onClick={this.onLogOut}
-              variant="contained"
-              color="#FFFFFF"
-              round="round"
-              style={{
+      <AppHeader brand="WiseWallet" links={<Button
+        onClick = {
+          this.onLogOut
+        }
+        variant = "contained"
+        color = "#FFFFFF"
+        round = "round"
+        style = {{
                 position: "relative",
                 float: "right",
                 color: "#092856",
                 backgroundColor: "#FFFFFF"
-              }}
-            >
-              Log Out
-            </Button>}
-          fixed="fixed"
-          color="primary"
-        />
-        <GridContainer
-          style={{
-            paddingLeft: "10%",
-            paddingRight: "10%",
-            paddingTop: "90px"
-          }}
-        >
-          <GridItem xs={12} sm={12} md={3}>
-            <Card classes={{ root: "card_style" }}>
-              <h3 className="impactScoreHeader">Total Impact Score</h3>
-              <div className="impactScore">
-                <h2 className="impactScoreNumber">{this.state.tScore}/100</h2>
-                <CircularProgress
-                  classes={{ colorPrimary: "impactScoreProgress", circle: "impactScoreProgress" }}
-                  color="primary"
-                  className="impactScoreProgress"
-                  variant="static"
-                  value={this.state.tScore}
-                  size={100}
-                />
-              </div>
+              }} > Log Out < /Button>} fixed="fixed" color="primary"/>
+      <GridContainer style={{
+          paddingLeft: "10%",
+          paddingRight: "10%",
+          paddingTop: "90px"
+        }}>
+        <GridItem xs={12} sm={12} md={3}>
+          <Card classes={{
+              root: "card_style"
+            }}>
+            <h3 className="impactScoreHeader">Total Impact Score</h3>
+            <div className="impactScore">
+              <h2 className="impactScoreNumber">{this.state.tScore}/100</h2>
+              <CircularProgress classes={{
+                  colorPrimary: "impactScoreProgress",
+                  circle: "impactScoreProgress"
+                }} color="primary" className="impactScoreProgress" variant="static" value={this.state.tScore} size={100}/>
+            </div>
 
-              <SnackbarContent
-                message={"People: " + this.state.sScore}
-                classes={{ root: "dashboard_snackbar" }}
-              />
-              <LinearProgress
-                variant="determinate"
-                color="primary"
-                value={this.state.sScore}
-                classes={{
-                  root: "linear_progress",
-                  barColorPrimary: "bar_color1"
-                }}
-              />
-              <SnackbarContent
-                message={"Planet: " + this.state.eScore}
-                classes={{ root: "dashboard_snackbar" }}
-              />
-              <LinearProgress
-                variant="determinate"
-                color="primary"
-                value={this.state.eScore}
-                classes={{
-                  root: "linear_progress",
-                  barColorPrimary: "bar_color2"
-                }}
-              />
-              <SnackbarContent
-                message={"Policy: " + this.state.gScore}
-                classes={{ root: "dashboard_snackbar" }}
-              />
-              <LinearProgress
-                variant="determinate"
-                color="primary"
-                value={this.state.gScore}
-                classes={{
-                  root: "linear_progress",
-                  barColorPrimary: "bar_color3"
-                }}
-              />
-              <SnackbarContent
-                message={"Politics: " + this.state.pScore}
-                classes={{ root: "dashboard_snackbar" }}
-              />
-              <LinearProgress
-                variant="determinate"
-                color="primary"
-                value={this.state.pScore}
-                classes={{
-                  root: "linear_progress last_linear_progress politicsLinearProgress",
-                  barColorPrimary: "bar_color4"
-                }}
-              />
-            </Card>
-          </GridItem>
-          <GridItem xs={12} sm={12} md={9}>
-            <div
-              style={{
-                overflowX: "auto",
-                marginTop: "30px",
-                borderRadius: "4px"
-              }}
-            >
-              {this.state.transactionList === null ? (
-                <div className="loading">
-                  <CircularProgress
-                    size={50}
-                    classes={{ colorSecondary: "table_loading" }}
-                    color="secondary"
-                    className={classes.progress}
-                  />
-                </div>
-              ) : (
-                <Table style={{ background: "white" }}>
+            <div data-tip="People: Whatever you want this to say"><SnackbarContent message={"People: " + this.state.sScore} classes={{
+                root: "dashboard_snackbar"
+              }}/>
+            <LinearProgress variant="determinate" color="primary" value={this.state.sScore} classes={{
+                root: "linear_progress",
+                barColorPrimary: "bar_color1"
+              }}/><ReactTooltip /></div>
+            <div data-tip="Planet: Whatever you want this to say"><SnackbarContent message={"Planet: " + this.state.eScore} classes={{
+                root: "dashboard_snackbar"
+              }}/>
+            <LinearProgress variant="determinate" color="primary" value={this.state.eScore} classes={{
+                root: "linear_progress",
+                barColorPrimary: "bar_color2"
+              }}/></div>
+            <div data-tip="Policy: Whatever you want this to say"><SnackbarContent message={"Policy: " + this.state.gScore} classes={{
+                root: "dashboard_snackbar"
+              }}/>
+            <LinearProgress variant="determinate" color="primary" value={this.state.gScore} classes={{
+                root: "linear_progress",
+                barColorPrimary: "bar_color3"
+              }}/></div>
+            <div data-tip="Politics: Whatever you want this to say"><SnackbarContent message={"Politics: " + this.state.pScore} classes={{
+                root: "dashboard_snackbar"
+              }}/>
+            <LinearProgress variant="determinate" color="primary" value={this.state.pScore} classes={{
+                root: "linear_progress last_linear_progress politicsLinearProgress",
+                barColorPrimary: "bar_color4"
+              }}/></div>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={12} md={9}>
+          <div style={{
+              overflowX: "auto",
+              marginTop: "30px",
+              borderRadius: "4px"
+            }}>
+            {
+              this.state.transactionList === null
+                ? (<div className="loading">
+                  <CircularProgress size={50} classes={{
+                      colorSecondary: "table_loading"
+                    }} color="secondary" className={classes.progress}/>
+                </div>)
+                : (<Table style={{
+                    background: "white"
+                  }}>
                   <TableHead>
                     <TableRow>
-                      <TableCell
-                        padding="default"
-                        classes={{ root: "table_cell" }}
-                      >
+                      <TableCell padding="default" classes={{
+                          root: "table_cell"
+                        }}>
                         Company
                       </TableCell>
                       <TableCell padding="dense">Date</TableCell>
@@ -225,12 +183,11 @@ class Dashboard extends React.Component {
                       <TableCell padding="dense">Politics</TableCell>
                     </TableRow>
                   </TableHead>
-                  {Object.keys(this.state.transactionList).map(t => (
-                    <TableRow>
-                      <TableCell
-                        padding="default"
-                        classes={{ root: "table_cell" }}
-                      >
+                  {
+                    Object.keys(this.state.transactionList).map(t => (<TableRow>
+                      <TableCell padding="default" classes={{
+                          root: "table_cell"
+                        }}>
                         {this.state.transactionList[t].name}
                       </TableCell>
                       <TableCell padding="dense">
@@ -251,20 +208,21 @@ class Dashboard extends React.Component {
                       <TableCell padding="dense">
                         {this.state.transactionList[t].pScore}
                       </TableCell>
-                    </TableRow>
-                  ))}
-                </Table>
-              )}
-            </div>
-            <Card color="info" style={{}}>
-              <CardBody color="info">
-                <h3>Suggested Alternatives Coming Soon</h3>
-              </CardBody>
-            </Card>
-          </GridItem>
-        </GridContainer>
-      </div>
-    );
+                    </TableRow>))
+                  }
+                </Table>)
+            }
+          </div>
+          <center>
+            <p style={{
+                fontSize: '20pt',
+                fontWeight: '600',
+                marginTop: '40px'
+              }}>Suggested Alternatives Coming Soon</p>
+          </center>
+        </GridItem>
+      </GridContainer>
+    </div>);
   }
 }
 
