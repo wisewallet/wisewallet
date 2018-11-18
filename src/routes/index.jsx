847e57d165c4a8af832a613ffa0bf5b65d12b819
William Brower <will@mywisewallet.com>
2018-11-18 13:02:22 -0500

    added privacy policy

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,6 +4,7 @@ import SignupPage from "views/SignupPage/SignupPage.jsx";
 import PlaidLink from "views/PlaidLink/PlaidLink.jsx";
 import Dashboard from "views/Dashboard/Dashboard.jsx";
 import AboutUsPage from "views/AboutUsPage/AboutUsPage.jsx";
+import PrivacyPage from "views/Privacy/PrivacyPage.jsx";
 import Charts from "views/Charts/Charts.jsx";
 var indexRoutes = [
 	{path: "/login", name: "Login", component: LoginPage},
@@ -12,6 +13,7 @@ var indexRoutes = [
 	{path: "/dashboard", name: "WiseWallet", component: Dashboard},
 	{path: "/charts", name: "WiseWallet", component: Charts},
 	{path: "/about", name: "WiseWallet", component: AboutUsPage},
+	{path: "/privacy", name: "WiseWallet", component: PrivacyPage},
 	{path: "/", name: "WiseWallet", component: LandingPage}
 ];
 
---
+++ src/views/Privacy/PrivacyPage.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+// @material-ui/icons
+import Favorite from "@material-ui/icons/Favorite";
+// core components
+import Header from "components/Header/Header.jsx";
+import GridContainer from "components/Grid/GridContainer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
+import Parallax from "components/Parallax/Parallax.jsx";
+// sections for this page
+import HeaderLinks from "components/Header/HeaderLinks.jsx";
+import SectionServices from "views/Privacy/Sections/SectionServices.jsx";
+
+import aboutUsStyle from "assets/jss/material-kit-pro-react/views/aboutUsStyle.jsx";
+const dashboardRoutes = [];
+class PrivacyPage extends React.Component {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+    document.body.scrollTop = 0;
+  }
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <Header color="transparent" routes={dashboardRoutes} brand="WiseWallet" links={<HeaderLinks dropdownHoverColor = "primary" />} fixed="fixed" changeColorOnScroll={{
+            height: 300,
+            color: "primary"
+          }}/>
+        <Parallax filter="dark" small>
+          <div className={classes.container}>
+            <GridContainer justify="center">
+              <GridItem
+                md={8}
+                sm={8}
+                className={classNames(
+                  classes.mlAuto,
+                  classes.mrAuto,
+                  classes.textCenter
+                )}
+              >
+                <h1 className={classes.title}>Privacy Policy</h1>
+              </GridItem>
+            </GridContainer>
+          </div>
+        </Parallax>
+        <div className={classNames(classes.main, classes.mainRaised)}>
+          <div className={classes.container}>
+            <SectionServices />
+          </div>
+        </div>
+        <div style={{
+            height: "100px", width: "100px"
+          }}>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withStyles(aboutUsStyle)(PrivacyPage);
