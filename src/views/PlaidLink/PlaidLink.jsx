2c88ea845b16547d612560df25098fc63762f26d
William Brower <will@mywisewallet.com>
2018-12-02 14:21:50 -0500

    Plaid Production Switch

--- src/views/PlaidLink/PlaidLink.jsx
+++ src/views/PlaidLink/PlaidLink.jsx
@@ -71,7 +71,7 @@ class Components extends React.Component {
                     <CardBody>
                       <GridContainer justify="center">
                         <GridItem xs={12} sm={5} md={5}>
-                          <PlaidLink clientName="WiseWallet" env="development" product={["transactions"]}
+                          <PlaidLink clientName="WiseWallet" env="production" product={["transactions"]}
                             publicKey="06812b585d6f3b0ebde352a7759bb1" onExit={this.handleOnExit}
                             onSuccess={this.handleOnSuccess}>
                             Open Link and connect your bank!
