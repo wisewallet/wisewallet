524531e96a5081af67a230fb41badd98db410972
So-Young Choi <34804400+soyoychoi@users.noreply.github.com>
2019-03-20 13:15:16 -0400

    Update LandingPage.jsx

--- src/views/LandingPage/LandingPage.jsx
+++ src/views/LandingPage/LandingPage.jsx
@@ -49,7 +49,7 @@ class LandingPage extends React.Component {
               </h4>
               <br/>
               <Button color="primary" size="lg" href="http://mywisewallet.com/signup" target="">
-                Sign up for our beta!
+                Get early access!
               </Button>
             </GridItem>
           </GridContainer>
