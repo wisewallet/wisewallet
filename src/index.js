06d8e17a275b5d6739ff6968785da9b842c2fe7b
Daniel Chain <DanielChain3@gmail.com>
2019-06-24 11:39:48 -0400

    update font for components

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router";
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 import indexRoutes from "./routes/index.jsx";
 
@@ -9,13 +10,24 @@ import "./assets/scss/material-kit-pro-react.css?v=1.1.0";
 
 var hist = createBrowserHistory();
 
+const THEME = createMuiTheme({
+   typography: {
+    "fontFamily": "gotham-regular",
+    "fontWeightLight": 300,
+    "fontWeightRegular": 400,
+    "fontWeightMedium": 500
+   }
+});
+
 ReactDOM.render(
+  <MuiThemeProvider theme={THEME}>
   <Router history={hist}>
     <Switch>
       {indexRoutes.map((prop, key) => {
         return <Route path={prop.path} key={key} component={prop.component} />;
       })}
     </Switch>
-  </Router>,
+  </Router>
+</MuiThemeProvider>,
   document.getElementById("root")
 );
